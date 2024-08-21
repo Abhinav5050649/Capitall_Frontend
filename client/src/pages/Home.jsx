b0eb16dec4f4195d1650ee9688f427a5639d150b
Abhinav Sharma <abhinav291sharma@gmail.com>
2024-08-21 09:37:43 +0530

    Update: Home page issue in build

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,18 +5,18 @@ export default function Home() {
     return (
         <>
             <Navbar />
-            <section className="bg-gray-900 text-white">
+            <section className="bg-gray-900">
                 <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen flex min-h-screen min-w-screen lg:items-center">
                     <div className="mx-auto max-w-3xl text-center">
                         <h1
-                            className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+                            className="bg-clip-text text-3xl text-white font-extrabold text-transparent sm:text-5xl"
                         >
                             XLO
 
-                            <span className="sm:block"> A Platform to Exchange Goods! </span>
+                            <span className="text-white sm:block"> A Platform to Exchange Goods! </span>
                         </h1>
 
-                        <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
+                        <p className="mx-auto mt-4 max-w-xl text-white sm:text-xl ">
                             Your one-stop destination for all-things.
                         </p>
 
